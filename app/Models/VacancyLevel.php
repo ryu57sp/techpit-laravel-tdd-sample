318acb6b7acee8ff07c60b0861da2e00587c0464
ryu57sp <ryuki.baske5@gmail.com>
2022-09-24 19:30:22 +0900

    LS-1, リファクタ前まで

---
+++ app/Models/VacancyLevel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+class VacancyLevel
+{
+    private $remainingCount;
+
+    public function __construct(int $remainingCount)
+    {
+        $this->remainingCount = $remainingCount;
+    }
+
+    public function mark(): string
+    {
+        if ($this->remainingCount === 0) {
+            return '×';
+        }
+
+        if ($this->remainingCount < 5) {
+            return '△';
+        }
+
+        return '◎';
+    }
+
+    public function slug(): string
+    {
+        if ($this->remainingCount === 0) {
+            return 'empty';
+        }
+
+        if ($this->remainingCount < 5) {
+            return 'few';
+        }
+
+        return 'enough';
+    }
+}
