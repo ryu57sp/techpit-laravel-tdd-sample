bd47c858ba61720483ce6cc4e5030391c324e0f2
ryu57sp <ryuki.baske5@gmail.com>
2022-10-22 19:04:49 +0900

    LS-2, フィーチャーテスト実装

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Lesson extends Model
 {
@@ -16,6 +17,11 @@ class Lesson extends Model
 
     private function remainingCount(): int
     {
-        return 0;
+        return $this->capacity - $this->reservations()->count();
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
